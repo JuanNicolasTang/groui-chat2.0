f5de4d5d1c34cef55e2bbc277691bbb4c9e5fcb1
JuanNicolasTang <nicolastnq@hotmail.com>
2025-09-27 11:48:48 -0500

    Delete uninstall.php

--- uninstall.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined('WP_UNINSTALL_PLUGIN')) { exit; }
-global $wpdb;
-delete_option('gpt5_sa_settings');
-$table = $wpdb->prefix . 'gpt5sa_embeddings';
-$wpdb->query("DROP TABLE IF EXISTS $table");
-$wpdb->query("DELETE FROM {$wpdb->options} WHERE option_name LIKE '_transient_gpt5sa_%' OR option_name LIKE '_transient_timeout_gpt5sa_%'");
